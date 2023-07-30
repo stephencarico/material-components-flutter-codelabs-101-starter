66c1d5903b211f060c4b765409c747fcc2ba14d9
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2023-07-30 13:00:20 -0600

    a bunch of stuff

--- mdc_100_series/lib/login.dart
+++ mdc_100_series/lib/login.dart
@@ -22,7 +22,9 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  // TODO: Add text editing controllers (101)
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,8 +42,40 @@ class _LoginPageState extends State<LoginPage> {
             ),
             const SizedBox(height: 120.0),
             // TODO: Remove filled: true values (103)
-            // TODO: Add TextField widgets (101)
-            // TODO: Add button bar (101)
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Username',
+              ),
+            ),
+            const SizedBox(height: 12.0),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            OverflowBar(
+                alignment: MainAxisAlignment.end,
+                // TODO: Add a beveled rectangular border to CANCEL (103)
+                children: <Widget>[
+                  TextButton(
+                    child: Text('CANCEL'),
+                    onPressed: () {
+                      _usernameController.clear();
+                      _passwordController.clear();
+                    },
+                  ),
+                  ElevatedButton(
+                    child: Text('NEXT'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ]),
           ],
         ),
       ),
