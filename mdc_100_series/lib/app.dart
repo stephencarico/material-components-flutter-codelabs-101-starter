debc4c520ace822e0a79e1167b7466163b41ffdc
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2023-07-31 21:40:56 -0600

    customize typography

--- mdc_100_series/lib/app.dart
+++ mdc_100_series/lib/app.dart
@@ -35,7 +35,6 @@ class ShrineApp extends StatelessWidget {
         // TODO: Pass _currentCategory for frontLayer (104)
         // TODO: Change backLayer field value to CategoryMenuPage (104)
       },
-      // TODO: Customize the theme (103)
       theme: _kShrineTheme,
     );
   }
@@ -46,14 +45,43 @@ final ThemeData _kShrineTheme = _buildShrineTheme();
 ThemeData _buildShrineTheme() {
   final ThemeData base = ThemeData.light(useMaterial3: true);
   return base.copyWith(
-    colorScheme: base.colorScheme.copyWith(
-      primary: kShrinePink100,
-      onPrimary: kShrineBrown900,
-      secondary: kShrineBrown900,
-      error: kShrineErrorRed,
-    ),
-    // TODO: Add the text themes (103)
-    // TODO: Decorate the inputs (103)
-  );
+      colorScheme: base.colorScheme.copyWith(
+        primary: kShrinePink100,
+        onPrimary: kShrineBrown900,
+        secondary: kShrineBrown900,
+        error: kShrineErrorRed,
+      ),
+      // TODO: Add the text themes (103)
+      textTheme: _buildShrineTextTheme(base.textTheme),
+      textSelectionTheme: const TextSelectionThemeData(
+        selectionColor: kShrinePink100,
+      )
+      // TODO: Decorate the inputs (103)
+      );
 }
+
 // TODO: Build a Shrine Text Theme (103)
+TextTheme _buildShrineTextTheme(TextTheme base) {
+  return base
+      .copyWith(
+        headlineSmall: base.headlineSmall!.copyWith(
+          fontWeight: FontWeight.w500,
+        ),
+        titleLarge: base.titleLarge!.copyWith(
+          fontSize: 18.0,
+        ),
+        bodySmall: base.bodySmall!.copyWith(
+          fontWeight: FontWeight.w400,
+          fontSize: 16.0,
+        ),
+        bodyLarge: base.bodyLarge!.copyWith(
+          fontWeight: FontWeight.w500,
+          fontSize: 16.0,
+        ),
+      )
+      .apply(
+        fontFamily: 'Rubik',
+        displayColor: kShrineBrown900,
+        bodyColor: kShrineBrown900,
+      );
+}
