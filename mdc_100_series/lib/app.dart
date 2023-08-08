850e863be719c628cbe439a3f995f2d117e1867f
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2023-08-07 20:20:00 -0600

    feat: add menu to the backlayer

--- mdc_100_series/lib/app.dart
+++ mdc_100_series/lib/app.dart
@@ -15,6 +15,7 @@
 import 'package:flutter/material.dart';
 
 import 'backdrop.dart';
+import 'category_menu_page.dart';
 import 'colors.dart';
 import 'home.dart';
 import 'login.dart';
@@ -22,8 +23,21 @@ import 'model/product.dart';
 import 'supplemental/cut_corners_border.dart';
 
 // TODO: Convert ShrineApp to stateful widget (104)
-class ShrineApp extends StatelessWidget {
+class ShrineApp extends StatefulWidget {
   const ShrineApp({Key? key}) : super(key: key);
+
+  @override
+  State<ShrineApp> createState() => _ShrineAppState();
+}
+
+class _ShrineAppState extends State<ShrineApp> {
+  Category _currentCategory = Category.all;
+
+  void _onCategoryTap(Category category) {
+    setState(() {
+      _currentCategory = category;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,14 +46,15 @@ class ShrineApp extends StatelessWidget {
       initialRoute: '/login',
       routes: {
         '/login': (BuildContext context) => const LoginPage(),
-        // TODO: Change to a Backdrop with a HomePage frontLayer (104)
         '/': (BuildContext context) => Backdrop(
-              // TODO: Make currentCategory field take _currentCategory (104)
-              currentCategory: Category.all,
+              currentCategory: _currentCategory,
               // TODO: Pass _currentCategory for frontLayer (104)
               frontLayer: HomePage(),
               // TODO: Change backLayer field value to CategoryMenuPage (104)
-              backLayer: Container(color: kShrinePink100),
+              backLayer: CategoryMenuPage(
+                currentCategory: _currentCategory,
+                onCategoryTap: _onCategoryTap,
+              ),
               frontTitle: Text('SHRINE'),
               backTitle: Text('MENU'),
             ),
